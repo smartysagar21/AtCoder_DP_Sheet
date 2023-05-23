1dc96ae7364f4af413bf9ef556db6b817354bae7
Shwet Sagar <105374331+smartysagar21@users.noreply.github.com>
2023-05-23 11:49:07 +0530

    Add files via upload

---
+++ DP.cpp
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+#define ll long long
+
+
+int main() {
+
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);cout.tie(NULL);
+
+	ll n;
+	cin>>n;
+
+	vector<ll>v(n);
+
+	for(ll i = 0; i < n; ++i){
+		cin>>v[i];
+	}
+
+	ll dp[n];
+	memset(dp, 0, sizeof(dp));
+
+	dp[0] = 0;
+	dp[1] = abs(v[0] - v[1]);
+
+	for(ll i = 2; i < n; i++){
+		dp[i] = dp[i - 1] + abs(v[i] - v[i-1]);
+		dp[i] = min(dp[i], dp[i - 2] + abs(v[i] - v[i - 2]));
+	}
+
+	cout<<dp[n - 1]<<endl;
+
+
+	return 0;
+}
+
